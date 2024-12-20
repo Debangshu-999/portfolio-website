686d611bc9e1eeb7de8c0b6a5635a900495ffd54
Debangshu Das <106870983+Debangshu-999@users.noreply.github.com>
2024-12-20 14:55:57 +0530

    Add files via upload

---
+++ sanity-backend/studio-personal_portfolio/schemaTypes/workExperience.ts
@@ -0,0 +1,29 @@
+import {defineField, defineType} from 'sanity'
+
+export const workExperience = defineType({
+  name: 'workExperience',
+  title: 'Work Experience',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      title: 'name',
+      type: 'string',
+    }),
+    defineField({
+      name: 'company',
+      title: 'Company',
+      type: 'string',
+    }),
+    defineField({
+      name: 'duration',
+      title: 'Duration',
+      type: 'string',
+    }),
+    defineField({
+      name: 'desc',
+      title: 'Desc',
+      type: 'string',
+    }),
+  ],
+})
